7a114f87ace39fc10442e9cca03095cfe2854ec1
apollock42 <99260918+apollock42@users.noreply.github.com>
2022-03-08 14:43:06 +0000

    Update Assignment2_embedded.ino.ino

--- Assignment2_embedded.ino/Assignment2_embedded.ino.ino
+++ Assignment2_embedded.ino/Assignment2_embedded.ino.ino
@@ -6,11 +6,13 @@ int pulseState =0;
 int task3Signal=2;
 int task4Signal=0;
 long frequency=0;
+int freq=0;
 long input4 = 0;
 int avAnalog=0;
 int error_code=2;
 int error=2;
 int T=0;
+int buttonState=0;
 #include <Ticker.h>
 #include <stdio.h>
 int i=0;
@@ -40,28 +42,28 @@ void cycle(){
   // 24 120 5 5 12 40 40 600
   test1();
   if (counter %100 == 0){
-    //test2();
+    buttonState=test2();
   }
   if (counter %500 ==0){
-    //test3();
+    //freq=test3();
   }
   if (counter %21 ==0){
-    t4Signal=test4();
+    //t4Signal=test4();
   }
   if (counter %21 ==1){
-    avAnalog=test5(t4Signal);
+    //avAnalog=test5(t4Signal);
   }
   if (counter %50 ==0){
     //test6();
   }
   if (counter %167 ==0){
-    error=test7(avAnalog);
+    //error=test7(avAnalog);
   }
   if (counter %167 ==1){
-    test8(error);
+    //test8(error);
   }
   if (counter %2500 ==0){
-    //test9();
+    //test9(buttonState,freq,avAnalog);
   }
 }
 
@@ -70,19 +72,22 @@ void test1(){
   delayMicroseconds(50);
   digitalWrite(ledRed,LOW);
 }
-void test2(){
+int test2(){
   pulseState=digitalRead(leftButt);
   if(pulseState==HIGH){
     Serial.println("task 2 button is being pressed");
+    return 1;
   }
   else{
     Serial.println("task 2 button is not being pressed");
+    return 0;
   }
 }
-void test3(){
+int test3(){
   T=pulseIn(task3Signal,LOW);
   frequency=1/(2*(T*pow(10,-6)));
-  Serial.println(frequency);
+  //Serial.println(frequency);
+  return frequency;
 }
 int test4(){
   input4 = analogRead(task4Signal);
@@ -123,7 +128,7 @@ int test7(int avAnalIn){
   
 }
 void test8(int error){
-  Serial.println(error);
+  //Serial.println(error);
   if (error==1){
     digitalWrite(ledGreen,HIGH);
   }
@@ -131,8 +136,12 @@ void test8(int error){
     digitalWrite(ledGreen,LOW);
   }
 }
-void test9(){
-  Serial.println("c");
+void test9(int buttonState, int freq, int average){
+  Serial.print(buttonState);
+  Serial.print(", ");
+  Serial.print(freq);
+  Serial.print(", ");
+  Serial.println(average);
 } 
 //}
 void loop() {
